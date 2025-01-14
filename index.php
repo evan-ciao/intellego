5193de153ab9a1bf612e47dae1185154cc9ef3eb
evan-ciao <ivanciaomh2000@gmail.com>
2025-01-14 09:35:50 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="UTF-8"/>
+
+  	<title>Hello, world!</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>
+	<meta name="description" content=""/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="icon" href="favicon.png">
+</head>
+<body>
+	<header>
+		<h1>intellego</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="index.php">test</a></li>
+		</ul>
+	</nav>
+	<main>
+		<p><?php echo 'Hello world!';?></p>
+		<p>
+
+There is an ancient Taoist story about a tree.
+
+A useless tree.
+
+The Useless Tree.
+
+There was an old and crooked oak tree by the village shrine, every branch twisted and gnarled. The tree was large enough to shade several thousand oxen and was a hundred spans around. It towered above the hilltops with its lowest branches eighty feet from the ground. More than ten of its branches were big enough to be made into boats. There were crowds of people around it, a marketplace. 
+
+Passing the old tree, Hui Tzu, a carpenter's apprentice said to Shih, the master carpenter, who without even turning his head, walked on without stopping:
+
+    “What a useless tree that is. Its trunk and branches were so crooked, so distorted and full of knots. The wood is so beautiful, but it cannot be cut up, no straight plank can be made from it. The tree serves no purpose at all.
+
+    There it stands beside the road. No carpenter will even look at it. Even you don't look at it master.” 
+
+Shih the master carpenter replied:
+
+    “The tree on the mountain height is its own enemy… The cinnamon tree is edible: so it is cut down! The lacquer tree is profitable: they maim it. Cherry, apple, pear, orange, lemon, pomelo, and other fruit trees. As soon as the fruit is ripe, the trees are stripped and abused. 
+
+    Their large branches are split, and the smaller ones torn off. Their life is bitter because of their usefulness. That is why they do not live out their natural lives but are cut off in their prime. They attract the attentions of the common world. This is so for all things.
+
+    That tree is useless. A boat made from it would sink, a coffin would soon rot, a tool would split, a door would ooze sap, and a beam would have termites. It's worthless timber and is of no use. That is why it has reached such a ripe old age.
+
+    Every man knows how useful it is to be useful. No one seems to know how useful it is to be useless.
+
+    This tree has been trying for a long time to be useless. It was almost destroyed several times. Finally it useless, and this is very useful.
+
+    So for this big tree, no use? It is planted in the wasteland, in emptiness. People walk idly around it, rest under its shadow. No axe or bill prepares its end. No one will ever cut it down.
+
+    Useless? You should worry!”
+
+The uselessness of this tree is the very thing that protected it.
+
+Good for nothing, nobody wanted it.
+
+So it remained, useless, uncut, growing older, becoming ancient, fullfilling it's own true nature, to be a useless tree.
+
+“No one seems to know how useful it is to be useless.”
+
+What is it to be useless?
+
+It is to be empty, free of striving to become something, to be anything special, without goal or attainment.
+
+To be useless is simply to relax, rest at ease, and allow our true nature to express itself in a simple, easy way.
+
+There is nothing to do, nothing to be, and nothing to attain.
+
+I'm nothing special. 
+
+You're nothing special.
+
+Everything we can perceive is impermanent, in the flow of life, constantly changing.
+
+If we free ourselves of goals and attainments, to truly let go of any wish to be a certain way, or to posess certain things or attributes, leaving everything alone, then we are truly Free.
+
+Without limit.
+
+Unbound and unbounded.
+
+This is true rest. 
+
+To be completely and utterly useless. 
+
+Like the tree.
+
+Life takes care of everything else.</p>
+	</main>
+</body>
+</html>
